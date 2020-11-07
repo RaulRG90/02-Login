df24bb0a638673e2b8a3d268c56da24d8373f615
Raul Retana <raul.dmai@gmail.com>
2020-11-07 08:29:12 -0600

    Update auth.guard.ts

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -6,20 +6,35 @@ import { AuthService } from '../services/auth.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  
+  valoy: boolean = false;
+
   constructor(private auth: AuthService,
               private router: Router){}
   //este se ejecuta cuando entra a las paginas protegida, esta en app-routing-module.ts
   canActivate(): boolean {
     //console.log('Guard');
 
+    this.auth.busquedadetoken().
+    subscribe(data => {
+      this.valoy = true;
+    });
+
     if ( this.auth.estaAutenticado() ) {
-      return true ;
-      
-    } else {
+      if ( this.valoy ) {
+        console.log('true guard busqueda');
+        return true ;
+      } else {
+        console.log('false guard busqueda');
+        this.router.navigateByUrl('/login');
+        return false;
+      }
+    }
+    if( !this.auth.estaAutenticado() ){
+      console.log('false guard esta autenticado');
       this.router.navigateByUrl('/login');
       return false;
       
     }
   }
-  
 }
